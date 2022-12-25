706332a1bf44bddfd3ac8f280369f97d3650d6d4
Sandesh-Hase <sandeshhase15@gmail.com>
2022-12-25 13:05:49 +0530

    added get_time

--- src/IPYNBrenderer/youtube.py
+++ src/IPYNBrenderer/youtube.py
@@ -4,3 +4,11 @@ from IPYNBrenderer.custom_exception import InvalidURLException
 from IPYNBrenderer.logger import logger
 from py_youtube import Data
 
+@ensure_annotations
+def get_time_info(URL: str) -> int:
+    def _verify_vid_id_len(vid_id, __expected_len=11):
+        len_of_vid_id = len(vid_id)
+        if len_of_vid_id != __expected_len:
+            raise InvalidURLException(
+                f"Invalid video id with length: {len_of_vid_id}, expected: {__expected_len}"
+            )
