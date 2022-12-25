7819958764141f6224a8600b5b621db7482726d8
Sandesh-Hase <sandeshhase15@gmail.com>
2022-12-25 13:09:02 +0530

    try in youtube

--- src/IPYNBrenderer/youtube.py
+++ src/IPYNBrenderer/youtube.py
@@ -12,3 +12,19 @@ def get_time_info(URL: str) -> int:
             raise InvalidURLException(
                 f"Invalid video id with length: {len_of_vid_id}, expected: {__expected_len}"
             )
+    try:
+        split_val = URL.split("=")
+        if len(split_val) > 3:
+            raise InvalidURLException
+        if "watch" in URL:
+            if "&t" in URL:
+                vid_id, time = split_val[-2][:-2], int(split_val[-1][:-1])
+                _verify_vid_id_len(vid_id)
+                logger.info(f"video starts at: {time}")
+                return time
+
+             else:
+                vid_id, time = split_val[-1], 0
+                _verify_vid_id_len(vid_id)
+                logger.info(f"video starts at: {time}")
+                return time
